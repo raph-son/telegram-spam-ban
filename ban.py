20dce37a2a404bfbe2c49985cdc0f2c8a6e4a804
Sam <48352522+raph-son@users.noreply.github.com>
2022-04-15 04:21:35 +0100

    Update ban.py
    This is a fully working code using module and methods completely different from the dev branch

--- ban.py
+++ ban.py
@@ -1,63 +1,110 @@
 """
-Ban users with names as admin or names match certain conditions
+Ban users with names as admin or names match certain conditions on entering
 """
-import sys
-from datetime import timedelta
+import logging
 
-from telethon import TelegramClient
-from telethon.tl.types import ChannelParticipantsAdmins
+from typing import Tuple, Optional
 
-import db
+from telegram import Update, Bot, Chat, ChatMember, ParseMode, ChatMemberUpdated
+from telegram.ext import Updater, CommandHandler, CallbackContext, ChatMemberHandler
 
-API_ID = 
-API_HASH = ""
-CHANNEL_ID = 
+from db import DB
 
-client = TelegramClient("client",  API_ID, API_HASH)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-async def main():
-    cmd = None
-    try:
-        cmd = [i for i in sys.argv[1].split(',')]
-    except IndexError:
-        pass
-    if cmd:
-        # add to sqlite
-        db.insert(cmd)
+db = DB()
 
-    tags = db.select()
-    tags = [tag[0] for tag in tags]
-    
-    admin_names = []
-    chat = await client.get_entity(CHANNEL_ID)
-    async for user in client.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        first_name = None
-        last_name = None
+CHAT_ID = 
+BOT_TOKEN = ''
+
+updater = Updater(token=BOT_TOKEN)
+dispatcher = updater.dispatcher
+
+bot = Bot(token=BOT_TOKEN)
+ADMINS = bot.get_chat_administrators(CHAT_ID)
+
+def get_admins():
+    names = set()
+    for admin in ADMINS:
+        if admin.user.first_name:
+            names.add(admin.user.first_name.lower())
+        if admin.user.last_name:
+            names.add(admin.user.last_name.lower())
+    return names
+
+def extract_status_change(chat_member_update: ChatMemberUpdated):
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
+
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = (
+        old_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    )
+    is_member = (
+        new_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+    )
+
+    return was_member, is_member
+
+def new_chat_members(update: Update, context: CallbackContext) -> None:
+    # New user in chat
+    result = extract_status_change(update.chat_member)
+    if result is None:
+        return
+
+    was_member, is_member = result
+    cause_name = update.chat_member.from_user.mention_html()
+    member_name = update.chat_member.new_chat_member.user.mention_html()
+
+    if not was_member and is_member:
+        ban_tags = db.select("tags", "tag")
+        user = update.chat_member.new_chat_member.user
+        first_name = ""
+        last_name = ""
         if user.first_name:
             first_name = user.first_name.lower()
         if user.last_name:
             last_name = user.last_name.lower()
-        admin_names.append([user.id, first_name, last_name])
+        if first_name or last_name:
+            admin_names = get_admins()
+            if (first_name in admin_names) or (last_name in admin_names):
+                # ban user
+                bot.ban_chat_member(CHAT_ID, user.id, until_date=0)
+            else:
+                for tag in ban_tags:
+                    tag = tag[0]
+                    if first_name == tag.lower() or last_name == tag.lower() or user.username.lower() == tag.lower():
+                        bot.ban_chat_member(CHAT_ID, user.id, until_date=0)
 
-    async for user in client.iter_participants(chat, aggressive=False):
-        status = True
-        for admin in admin_names:
-            if user.id != admin[0]: # Only look into non-admins
-                if user.first_name or user.last_name:
-                    if ((user.first_name == admin[1]) and (user.last_name == admin[2]) or (user.first_name == admin[2]) and (user.last_name == admin[1])):
-                        # ban user
-                        await client.edit_permissions(chat, user, timedelta(minutes=0),
-                              view_messages=False)
-                        status = False
-                        break
-        if status: # User pass admin check
-            if user.first_name or user.last_name:
-                if user.username.lower() in tags or user.first_name in tags or user.last_name in tags:
-                    await client.edit_permissions(chat, user, timedelta(minutes=0), view_messages=False)
-                    break
-    db.close()
+def admin(update: Update, context: CallbackContext):
+    if update.message.from_user.id in [admin.user.id for admin in ADMINS]: # Make sure only admin can use Bot
+        tags = update.message.text.split(" [") # example /admin ['admin', 'support']
+        if len(tags) == 2:
+            tags = tags[1].strip('][').strip("'").strip('"').split(', ')
+            for tag in tags:
+                db.insert(tag.replace("'",""), "tags", "tag")
 
+start_handler = CommandHandler('admin', admin)
+dispatcher.add_handler(start_handler)
 
-with client:
-    client.loop.run_until_complete(main())
+dispatcher.add_handler(ChatMemberHandler(new_chat_members, ChatMemberHandler.CHAT_MEMBER))
 
+updater.start_polling(allowed_updates=Update.ALL_TYPES)
+
+updater.idle()
